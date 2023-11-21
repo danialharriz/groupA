3834ae6bd8a6b52901e91c43992bdf16f69b80dd
LOO JIA CHANG <129271114+jcl03@users.noreply.github.com>
2023-11-21 12:41:01 +0800

    Update DataManagementSystemYouthVenture.sql

--- DataManagementSystemYouthVenture/DataManagementSystemYouthVenture.sql
+++ DataManagementSystemYouthVenture/DataManagementSystemYouthVenture.sql
@@ -15,18 +15,33 @@ CREATE TABLE IF NOT EXISTS `Users` (
   PRIMARY KEY (`UserID`)
 );
 
-CREATE TABLE IF NOT EXISTS `Institute`(
-    `InstituteID` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Organization`(
+    `OrganizationID` INT NOT NULL AUTO_INCREMENT,
     `Name` VARCHAR(45) NULL,
     `Address` VARCHAR(45) NULL,
     `City` VARCHAR(45) NULL,
     `State` VARCHAR(45) NULL,
-    `Zip` INT NULL,
+    `Organiser` INT NULL,
     `Website` VARCHAR(45) NULL,
     `ContactEmail` VARCHAR(45) NULL,
     `ContactPhone` INT NULL,
-    PRIMARY KEY (`InstituteID`)
+    PRIMARY KEY (`OrganizationID`)
 );
+
+CREATE TABLE IF NOT EXISTS `Company`(
+    `CompanyID` INT NOT NULL AUTO_INCREMENT,
+    `OrganizationID` INT NOT NULL,
+    PRIMARY KEY (`CompanyID`),
+    FOREIGN KEY (`OrganizationID`) REFERENCES `Organization`(`OrganizationID`)
+);
+
+CREATE TABLE IF NOT EXISTS `Institute`(
+    `InstituteID` INT NOT NULL AUTO_INCREMENT,
+    `OrganizationID` INT NOT NULL,
+    PRIMARY KEY (`InstituteID`),
+    FOREIGN KEY (`OrganizationID`) REFERENCES `Organization`(`OrganizationID`)
+);
+
 
 CREATE TABLE IF NOT EXISTS `Course`(
     `CourseID` INT NOT NULL AUTO_INCREMENT,
@@ -50,11 +65,21 @@ CREATE TABLE IF NOT EXISTS `Student`(
 CREATE TABLE IF NOT EXISTS `Event`(
     `EventID` INT NOT NULL AUTO_INCREMENT,
     `Name` VARCHAR(45) NULL,
-    `Date` DATE NULL,
-    `Time` TIME NULL,
+    `Description` TEXT NULL,
+    `DateAndTime` DATETIME NULL,
     `Location` VARCHAR(45) NULL,
-    `Description` VARCHAR(45) NULL,
-    PRIMARY KEY (`EventID`)
+    `Organizer` INT NULL,
+    `EventType` INT NULL, /* 0 for workshop, 1 for competition, 2 for seminar, 3 for talk */
+    `RewardPoints` INT NULL,
+    `InstituteID` INT NULL,
+    `CompanyID` INT NULL,
+    `OrganizationID` INT NULL,
+    `UserID` INT NULL,
+    PRIMARY KEY (`EventID`),
+    FOREIGN KEY (`UserID`) REFERENCES `Users`(`UserID`),
+    FOREIGN KEY (`InstituteID`) REFERENCES `Institute`(`InstituteID`),
+    FOREIGN KEY (`CompanyID`) REFERENCES `Company`(`CompanyID`),
+    FOREIGN KEY (`OrganizationID`) REFERENCES `Organization`(`OrganizationID`)
 );
 
 CREATE TABLE IF NOT EXISTS `Participants`(
@@ -86,101 +111,11 @@ CREATE TABLE IF NOT EXISTS `Lecturer`(
     PRIMARY KEY (`StaffID`,`InstituteID`)
 );
 
-CREATE TABLE IF NOT EXISTS `PartnerCompany`(
-    `PartnerCompanyID` INT NOT NULL AUTO_INCREMENT,
-    `Name` VARCHAR(45) NULL,
-    `Address` VARCHAR(45) NULL,
-    `City` VARCHAR(45) NULL,
-    `State` VARCHAR(45) NULL,
-    `Zip` INT NULL,
-    `Website` VARCHAR(45) NULL,
-    `ContactEmail` VARCHAR(45) NULL,
-    `ContactPhone` INT NULL,
-    PRIMARY KEY (`PartnerCompanyID`)
-);
-
-CREATE TABLE IF NOT EXISTS `PartnerCompanyStaff`(
-    `UserID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `CompanyStaff`(
     `StaffID` INT NOT NULL,
-    `PartnerCompanyID` INT NOT NULL,
-    FOREIGN KEY (`UserID`) REFERENCES `Users`(`UserID`),
-    FOREIGN KEY (`PartnerCompanyID`) REFERENCES `PartnerCompany`(`PartnerCompanyID`),
-    PRIMARY KEY (`UserID`,`PartnerCompanyID`)
-);
-
-CREATE TABLE IF NOT EXISTS `UserPost`(
-    `PostID` INT NOT NULL AUTO_INCREMENT,
     `UserID` INT NOT NULL,
-    `Post` VARCHAR(255) NULL,
+    `CompanyID` INT NOT NULL,
     FOREIGN KEY (`UserID`) REFERENCES `Users`(`UserID`),
-    PRIMARY KEY (`PostID`)
-);
-
-CREATE TABLE IF NOT EXISTS `Post_Attachment`(
-    `AttachmentID` INT NOT NULL AUTO_INCREMENT,
-    `PostID` INT NOT NULL,
-    `Attachment` VARCHAR(255) NULL,
-    FOREIGN KEY (`PostID`) REFERENCES `UserPost`(`PostID`),
-    PRIMARY KEY (`AttachmentID`)
-);
-
-CREATE TABLE IF NOT EXISTS `UserPostComment`(
-    `CommentID` INT NOT NULL AUTO_INCREMENT,
-    `PostID` INT NOT NULL,
-    `UserID` INT NOT NULL,
-    `Comment` VARCHAR(255) NULL,
-    FOREIGN KEY (`PostID`) REFERENCES `UserPost`(`PostID`),
-    FOREIGN KEY (`UserID`) REFERENCES `Users`(`UserID`),
-    PRIMARY KEY (`CommentID`)
-);
-
-CREATE TABLE IF NOT EXISTS `UserPostCommentReply`(
-    `ReplyID` INT NOT NULL AUTO_INCREMENT,
-    `CommentID` INT NOT NULL,
-    `UserID` INT NOT NULL,
-    `Reply` VARCHAR(255) NULL,
-    FOREIGN KEY (`CommentID`) REFERENCES `UserPostComment`(`CommentID`),
-    FOREIGN KEY (`UserID`) REFERENCES `Users`(`UserID`),
-    PRIMARY KEY (`ReplyID`)
-);
-
-CREATE TABLE IF NOT EXISTS `UserPostLike`(
-    `LikeID` INT NOT NULL AUTO_INCREMENT,
-    `PostID` INT NOT NULL,
-    `UserID` INT NOT NULL,
-    FOREIGN KEY (`PostID`) REFERENCES `UserPost`(`PostID`),
-    FOREIGN KEY (`UserID`) REFERENCES `Users`(`UserID`),
-    PRIMARY KEY (`LikeID`)
-);
-
-CREATE TABLE IF NOT EXISTS `Staff`(
-    `UserID` INT NOT NULL,
-    `StaffID` INT NOT NULL,
-    `Type` INT NOT NULL, /* 0 for full time, 1 for part time */
-    `Active` INT NOT NULL, /* 0 for inactive, 1 for active */
-    `JobTitle` VARCHAR(255) NULL,
-    FOREIGN KEY (`UserID`) REFERENCES `Users`(`UserID`),
-    PRIMARY KEY (`StaffID`)
-);
-
-CREATE TABLE IF NOT EXISTS `JobHistory`(
-    `JobHistoryID` INT NOT NULL AUTO_INCREMENT,
-    `StaffID` INT NOT NULL,
-    `PartnerCompanyID` INT NOT NULL,
-    `JobTitle` VARCHAR(255) NULL,
-    `StartDate` DATE NOT NULL,
-    `EndDate` DATE NULL,
-    FOREIGN KEY (`StaffID`) REFERENCES `Staff`(`StaffID`),
-    FOREIGN KEY (`PartnerCompanyID`) REFERENCES `PartnerCompany`(`PartnerCompanyID`),
-    PRIMARY KEY (`JobHistoryID`)
-);
-
-CREATE TABLE IF NOT EXISTS `PartnerCompanyJobHistory`(
-    `JobHistoryID` INT NOT NULL AUTO_INCREMENT,
-    `PartnerCompanyID` INT NOT NULL,
-    `JobTitle` VARCHAR(255) NULL,
-    `StartDate` DATE NOT NULL,
-    `EndDate` DATE NULL,
-    FOREIGN KEY (`PartnerCompanyID`) REFERENCES `PartnerCompany`(`PartnerCompanyID`),
-    PRIMARY KEY (`JobHistoryID`)
+    FOREIGN KEY (`CompanyID`) REFERENCES `Company`(`CompanyID`),
+    PRIMARY KEY (`StaffID`,`CompanyID`)
 );
