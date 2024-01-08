037514656cded5992478fa3a3b0615eadee37e6a
harriz <danial.harriz360@gmail.com>
2024-01-08 15:38:44 +0800

    .

--- mvcloginregister/app/views/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-   //require APPROOT . '/views/includes/head.php';
-?>
-<div id="section-landing">
-    <?php
-       //require APPROOT . '/views/includes/navigation.php';
-       require APPROOT . '/views/users/login.php';
-    ?>
-</div>
